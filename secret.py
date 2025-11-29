4a722266fe4cbe2693d51b4bad4801767b88e366
TOMprojet <155451964+TOMprojet@users.noreply.github.com>
2025-11-29 17:37:25 +0100

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,7 +1,7 @@
 ACCOUNTS = {
     "bitget1": {
-        "public_api": "bg_040c77e0d4fa2fb4d0bfc547a7f3bd13",
-        "secret_api": "bd263d0e4cb9efc08d16ebe93d35022389ab6b6366e19b6f9612f39d41a165b2",
+        "public_api": "bg_34814e7e2b323ecee2db2c85240b013b",
+        "secret_api": "197f0ddaf808ba586109bfb65125606ce495db54e4abfb6152c801009f5d8062",
         "password": "TOMapi95",
     },
 }
